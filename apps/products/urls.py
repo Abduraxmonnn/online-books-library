f9b595e4cde9dcf1945e0a3421a4914995862aca
Abduraxmon Asatullayev <abduraxmonasatullayev35@gmail.com>
2023-01-25 20:51:38 +0500

    changed APIView to ModelViewSet in products

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.products.views import ProductListAPIView, ProductDetailAPIView
+from apps.products.views import ProductListViewSet, ProductDetailAPIView
 
 urlpatterns = [
-    path(r'list/', ProductListAPIView.as_view()),
+    path(r'list/', ProductListViewSet.as_view({'get': 'list'})),
     path(r'detail/<int:pk>/', ProductDetailAPIView.as_view()),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -2,7 +2,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Rest Framework
-from rest_framework import views
+from rest_framework import views, viewsets
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -12,22 +12,15 @@ from apps.products.serializers import ProductSerializer, ProductDetailSerializer
 from apps.services import list_objects_by_select_related, get_object_by_id_or_404_and_by_prefetch_related
 
 
-class ProductListAPIView(views.APIView):
+class ProductListViewSet(viewsets.ModelViewSet):
+    queryset = list_objects_by_select_related(Product.objects, 'author')
+    serializer_class = ProductSerializer
+
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ["name", "category__name", "author__name"]
     filterset_fields = ["name", "category", "year", "language", "created_date"]
     ordering_fields = ["name", "year", "-id"]
     ordering = ['-id']
-
-    def filter_queryset(self, queryset):
-        for backend in list(self.filter_backends):
-            queryset = backend().filter_queryset(self.request, queryset, self)
-        return queryset
-
-    def get(self, request):
-        queryset = self.filter_queryset(list_objects_by_select_related(Product.objects, 'author'))
-        serializer = ProductSerializer(queryset, many=True).data
-        return Response(serializer)
 
 
 class ProductDetailAPIView(views.APIView):

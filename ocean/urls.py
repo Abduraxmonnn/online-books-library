e538ddb9bc04b855586d7992fe654d014ea9817f
Abdurakhmon <90904737+Abduraxmonnn@users.noreply.github.com>
2024-07-25 19:54:13 +0500

    Update urls.py

--- ocean/urls.py
+++ ocean/urls.py
@@ -10,9 +10,5 @@ urlpatterns = (
     path(r'main/', include('apps.urls'),  name='main')
 )
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns += path('__debug__/', include('debug_toolbar.urls'))
-
 urlpatterns += tuple(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 urlpatterns += tuple(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
